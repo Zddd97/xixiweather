4260b1a0676dc0cd5d4a8c2ecd02c5854866a26d
Zddd97 <245563617@qq.com>
2017-07-15 17:26:33 +0800

    创建数据库和表的配置，省市县的三级列表。

---
+++ app/src/main/java/com/example/leon/xixiweather/util/Utility.java
@@ -0,0 +1,79 @@
+package com.example.leon.xixiweather.util;
+
+import android.text.TextUtils;
+
+import com.example.leon.xixiweather.db.City;
+import com.example.leon.xixiweather.db.County;
+import com.example.leon.xixiweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Leon on 2017/7/15.
+ */
+
+public class Utility {
+    public  static    boolean handleProvinceResponse(String response) throws JSONException {
+        if (!TextUtils.isEmpty(response)){
+            try {
+            JSONArray allProvinces = new JSONArray(response);
+            for (int i = 0; i <allProvinces.length() ; i++) {
+                JSONObject provinceObject = allProvinces.getJSONObject(i);
+                Province province = new Province();
+                province.setProvinceName(provinceObject.getString("name"));
+                province.setProvinceCode(provinceObject.getInt("id"));
+                province.save();
+
+
+            }  return true;
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        }
+    return  false;
+    }
+    public  static   boolean handleCityResponse(String response, int provinId) throws JSONException {
+        if (!TextUtils.isEmpty(response)){
+            try {
+            JSONArray allCities = new JSONArray(response);
+            for (int i = 0; i <allCities.length() ; i++) {
+                JSONObject cityObject= allCities.getJSONObject(i);
+                City city = new City();
+                city.setCityNmae(cityObject.getString("name"));
+                city.setCityCode(cityObject.getInt("id"));
+                city.setProvinceId(provinId);
+                city.save();
+
+
+            }return  true;
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        }
+        return  false;
+    }
+    public  static    boolean handleCountyResponse(String response,int cityId) throws JSONException {
+        if (!TextUtils.isEmpty(response)){try {
+
+
+            JSONArray allCounties = new JSONArray(response);
+            for (int i = 0; i < allCounties.length(); i++) {
+                JSONObject countyObject = allCounties.getJSONObject(i);
+                County county = new County();
+                county.setCountyNmae(countyObject.getString("name"));
+                county.setCityId(cityId);
+                county.save();
+
+
+            }
+            return true;
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        }
+        return  false;
+    }
+
+}
