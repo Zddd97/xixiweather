fb45ed9b7d91b8ad7956b7de7f1559ad92e8ffc3
Zddd97 <245563617@qq.com>
2017-07-17 11:34:36 +0800

    查看天气信息逻辑实现

--- app/src/main/java/com/example/leon/xixiweather/util/Utility.java
+++ app/src/main/java/com/example/leon/xixiweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.leon.xixiweather.db.City;
 import com.example.leon.xixiweather.db.County;
 import com.example.leon.xixiweather.db.Province;
+import com.example.leon.xixiweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -75,5 +77,16 @@ public class Utility {
         }
         return  false;
     }
+public  static Weather handleWeatherResponse(String response){
 
+    try {
+        JSONObject jsonObject=new JSONObject(response);
+        JSONArray jsonArray =jsonObject.getJSONArray("HeWeather");
+        String weatherContent =jsonArray.getJSONObject(0).toString();
+        return  new Gson().fromJson(weatherContent ,Weather.class);
+    } catch (JSONException e) {
+        e.printStackTrace();
+    }
+    return null;
+}
 }
