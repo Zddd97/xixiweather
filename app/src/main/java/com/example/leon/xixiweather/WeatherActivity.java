219a93f1ce537d0be8054af2f8a1450dfd1ecb05
Zddd97 <245563617@qq.com>
2017-07-18 11:00:34 +0800

    城市切换和手动更新

--- app/src/main/java/com/example/leon/xixiweather/WeatherActivity.java
+++ app/src/main/java/com/example/leon/xixiweather/WeatherActivity.java
@@ -4,11 +4,15 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ScrollingView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -42,11 +46,19 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
     private  TextView sportText;
     private ImageView bingPicImg;
+    public SwipeRefreshLayout swipeRefesh;
+    public DrawerLayout drawerlayout;
+    private Button navButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        drawerlayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton= (Button) findViewById(R.id.nav_button);
+
+        swipeRefesh= (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefesh.setColorSchemeResources(R.color.colorPrimary);
         weatherLayout= (ScrollView) findViewById(R.id.weather_layout);
         titleCity= (TextView) findViewById(R.id.title_city);
         titleUpdateTime= (TextView) findViewById(R.id.title_update_time);
@@ -59,17 +71,31 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText= (TextView) findViewById(R.id.car_wash_text);
         sportText= (TextView) findViewById(R.id.sport_text);
         bingPicImg= (ImageView) findViewById(R.id.bing_pic_img);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerlayout.openDrawer(GravityCompat.START);
+            }
+        });
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString =prefs.getString("weather",null);
+        final String weatherId;
         if (weatherString!=null){
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            weatherId=weather.basic.weatherId;
             showWeatherInfo(weather);
         }else{
-            String weatherId= getIntent().getStringExtra("weather_id");
+           weatherId= getIntent().getStringExtra("weather_id");
           weatherLayout.setVisibility(View.INVISIBLE);
            requestWeather(weatherId);
 
         }
+        swipeRefesh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(weatherId);
+            }
+        });
         String bingPic =prefs.getString("bing_pic",null);
         if (bingPic!=null){
             Glide.with(this).load(bingPic).into(bingPicImg);
@@ -121,7 +147,9 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                        swipeRefesh.setRefreshing(false);
                     }
+
                 });
                 loadBingPic();
             }
@@ -141,6 +169,7 @@ public class WeatherActivity extends AppCompatActivity {
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefesh.setRefreshing(false);
                     }
                 });
             }
